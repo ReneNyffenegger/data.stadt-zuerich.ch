b2df69f7212e1a4e8cebb75030e627cafd97b371
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-06-05 10:13:20 +0200

    + sd_zv_kitas_stadtquartier

--- load/db/MS-SQL/create-schema.sql
+++ load/db/MS-SQL/create-schema.sql
@@ -8,12 +8,42 @@ go
 drop   table if exists dbo.sd_zv_kitas_schulkreis;
 go
 
+drop   table if exists dbo.sd_zv_kitas_stadtquartier_imp;
+go
+
+drop   table if exists dbo.sd_zv_kitas_stadtquartier;
+go
+
 create table dbo.sd_zv_kitas_schulkreis (
-   jahr             integer     , -- integer,
-   skname           varchar(200),
-   anz_bp_sq        float,
+   jahr             integer     ,
+   skname           varchar( 50),
+   anz_bp_sk        float,
    anz_kinder_va    float,
    belegungsfaktor  float,
+   versorgungsquote float
+);
+go
+
+create table dbo.sd_zv_kitas_stadtquartier_imp (
+   qnr              varchar(100), -- integer     ,
+   qname            varchar(100), -- varchar( 50),
+   skname           varchar(100), -- varchar( 50),
+   jahr             varchar(100), -- integer     ,
+   anz_bp_sk        varchar(100), -- float       ,
+   anz_kinder_va    varchar(100), -- float       ,
+   belegungsfaktor  varchar(100), -- float       ,
+   versorgungsquote varchar(100), -- float
+);
+go
+
+create table dbo.sd_zv_kitas_stadtquartier (
+   qnr              integer     ,
+   qname            varchar( 50),
+   skname           varchar( 50),
+   jahr             integer     ,
+   anz_bp_sk        float       ,
+   anz_kinder_va    float       ,
+   belegungsfaktor  float       ,
    versorgungsquote float
 );
 go
--- load/db/MS-SQL/import-data.sql
+++ load/db/MS-SQL/import-data.sql
@@ -7,15 +7,51 @@
 use st_zh;
 go
 
-bulk insert dbo.sd_zv_kitas_schulkreis
-from '$(curDir)\..\..\..\dataset\sd_zv_kitas_schulkreis\sd_zv_kitas_schulkreis.csv'
+-- bulk insert dbo.sd_zv_kitas_schulkreis
+--    from '$(curDir)\..\..\..\dataset\sd_zv_kitas\sd_zv_kitas_schulkreis.csv'
+-- -- from 'C:\Users\OMIS.Rene\github\github\data.stadt-zuerich.ch\dataset\sd_zv_kitas\sd_zv_kitas_schulkreis.csv'
+-- with (
+--    firstRow        =      2 ,
+--    lastRow         =    118 ,
+--    codepage        = '65001',
+--    format          = 'CSV'  ,
+--    fieldterminator = ','   
+-- -- rowTerminator   = '0x0a',
+-- -- DATAFILETYPE    = 'char'
+-- );
+-- go
+
+
+bulk insert dbo.sd_zv_kitas_stadtquartier_imp
+from '$(curDir)\..\..\..\dataset\sd_zv_kitas\sd_zv_kitas_stadtquartier.csv'
 with (
    firstRow        =      2 ,
-   lastRow         =    109 ,
+   lastRow         =    174 ,
    codepage        = '65001',
    format          = 'CSV'  ,
    fieldterminator = ','   
--- rowTerminator   = '0x0a',
--- DATAFILETYPE    = 'char'
 );
+go
+
+insert into dbo.sd_zv_kitas_stadtquartier
+-- insert into dbo.sd_zv_kitas_stadtquartier
+   select
+      case when i.qnr       = '.' then null else i.qnr       end,
+      i.qname            ,
+      i.skname           ,
+      i.jahr             ,
+      case when i.anz_bp_sk = '.' then null else i.anz_bp_sk end,
+      i.anz_kinder_va    ,
+      i.belegungsfaktor  ,
+      i.versorgungsquote  
+   from
+      dbo.sd_zv_kitas_stadtquartier_imp i;
+--    openrowset (
+--       bulk         '$(curDir)\..\..\..\dataset\sd_zv_kitas\sd_zv_kitas_stadtquartier.csv',
+--       formatfile = '$(curDir)\sd_zv_kitas_stadtquartier.fmt',
+--       firstRow   =      2,
+--       lastRow    =    174,
+--       codepage   = '65001'
+--    ) as i;
+-- 
 go
